9cedd5b44d847cb841cf391958b5ef65de6009c0
Madhuvanthi-78 <madhu.vd5n@gmail.com>
2025-07-24 11:18:56 +0530

    Update ExpenseChart.js

--- src/components/ExpenseChart.js
+++ src/components/ExpenseChart.js
@@ -1,1 +1,42 @@
+import React, { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
+import { Pie } from 'react-chartjs-2';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+export const ExpenseChart = () => {
+  const { transactions } = useContext(GlobalContext);
+
+  const expenseByCategory = transactions.reduce((acc, curr) => {
+    const cat = curr.category || 'General';
+    acc[cat] = (acc[cat] || 0) + Math.abs(curr.amount);
+    return acc;
+  }, {});
+
+  const data = {
+    labels: Object.keys(expenseByCategory),
+    datasets: [
+      {
+        data: Object.values(expenseByCategory),
+        backgroundColor: [
+          '#FF6384',
+          '#36A2EB',
+          '#FFCE56',
+          '#8BC34A',
+          '#FF9800',
+          '#9C27B0',
+          '#03A9F4'
+        ],
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <h3>Expenses by Category</h3>
+      <Pie data={data} />
+    </div>
+  );
+};
 
