faf23f535ea7923142c4f87df9ababd2b5abb545
Madhuvanthi-78 <madhu.vd5n@gmail.com>
2025-07-24 11:15:29 +0530

    Update TransactionList.js

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,17 +1,20 @@
 import React, { useContext } from 'react';
-import { Transaction } from './Transaction';
-
 import { GlobalContext } from '../context/GlobalState';
 
 export const TransactionList = () => {
-  const { transactions } = useContext(GlobalContext);
+  const { transactions, deleteTransaction } = useContext(GlobalContext);
 
   return (
     <>
       <h3>History</h3>
       <ul className="list">
-        {transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction} />))}
+        {transactions.map(transaction => (
+          <li key={transaction.id} className={transaction.amount < 0 ? 'minus' : 'plus'}>
+            {transaction.text} <span>{transaction.category}</span> <span>{transaction.amount < 0 ? '-' : '+'}${Math.abs(transaction.amount)}</span>
+            <button onClick={() => deleteTransaction(transaction.id)} className="delete-btn">x</button>
+          </li>
+        ))}
       </ul>
     </>
-  )
-}
+  );
+};
